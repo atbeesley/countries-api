c9b6d707eda3df1a320d50bb48d6159cf6da4d05
user <you@exampalex.com>
2019-10-22 14:43:58 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: "#app",
+    data:{
+      countries: []
+    },
+    mounted(){
+      this.fetchCountries();
+    },
+    computed: {
+      totalPopulations: function(){
+        return this.countries.reduce((runningTotal, country) => runningTotal + country.population, 0)
+      }
+    },
+    methods: {
+      fetchCountries: function (){
+          fetch("https://restcountries.eu/rest/v2/all")
+          .then((response) => response.json())
+          .then(data => this.countries = data)
+          // .then(data.reduce((runningTotal, country) => runningTotal + country.population, 0))
+      }
+    }
+  })
+})
